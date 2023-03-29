8e74b300d1f1475f70c21fe0d1a195ac706b23cc
efoosag <efoosagsite@gmail.com>
2023-03-29 06:38:09 +0100

    add password column to users table

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,9 @@ class User < ApplicationRecord
   has_many :articles
   validates :username, presence: true, uniqueness: { case_sensitive: false }, 
                                   length: { minimum: 3, maximun: 25}
-                                  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: { case_sensitive: false }, 
                                   length: { minimum: 3, maximun: 25},
                                   format: { with: VALID_EMAIL_REGEX }
+  has_secure_password
 end
