04fa89af94b50936827e90cb846d2fc8a2d7f88b
efoosag <efoosagsite@gmail.com>
2023-04-03 15:10:15 +0100

    add logging and session management

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,15 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update, :show]
+
   def show
-    @user = User.find(params[:id])
     @articles = @user.articles.paginate(page: params[:page], per_page: 4)
   end
 
   def index
     @users = User.paginate(page: params[:page], per_page: 4)
+  end
+  
+  def edit    
   end
 
   def new
@@ -15,6 +19,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Welcome to Allsport, #{@user.username} have been successfully signed up "
       redirect_to articles_path
     else
@@ -22,12 +27,7 @@ class UsersController < ApplicationController
     end
   end
 
-  def edit
-    @user = User.find(params[:id])
-  end
-
-  def update
-    @user = User.find(params[:id])
+  def update    
     if @user.update(user_params)
       flash[:notice] = "#{@user.username}, your profile have been successfully updated"
       redirect_to user_path(@user)
@@ -41,5 +41,9 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
